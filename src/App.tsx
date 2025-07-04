7e695080febd5bfb6b6dea0bb742358e8e0ee1ce
Dean Zaballero <126765808+Feyded@users.noreply.github.com>
2025-07-04 10:09:47 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import imageUrl from "./assets/fuck-you-removebg-preview.png";
+
+function App() {
+  const [showImage, setShowImage] = useState(false);
+
+  return (
+    <div className="container">
+      {!showImage ? (
+        <button onClick={() => setShowImage(true)} className="open-btn">
+          Pa testing Paolo
+        </button>
+      ) : (
+        <img src={imageUrl} alt="Replaced" className="fade-in-wiggle " />
+      )}
+    </div>
+  );
+}
+
+export default App;
